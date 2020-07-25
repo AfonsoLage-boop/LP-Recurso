a65b2016e1f5e1938c1dfab8fd479ba074d2eff2
AfonsoLage-boop <afomsosantoslage@gmail.com>
2020-07-26 00:32:16 +0100

    Start Agent.cs

---
+++ VirusSim/Agent.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace VirusSim
+{
+    public class Agent
+    {
+        public int ID {get ; }
+
+        public Coords Pos { get ; private set; }
+
+        public State HeIs {get; private set; }
+
+        private Grid grid;
+
+        public Agent ( Coords Pos, State HeIs, Grid grid)
+        {
+            Pos = pos;
+            HeIs = heis;
+            
+            
+
+
+        }
+
+
+
+    }
+}
