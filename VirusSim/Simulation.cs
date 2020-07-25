754f46cc30551c0f42e6344840c11ef24270ef1f
andrepucas <andre.pucas.santos@gmail.com>
2020-07-25 23:06:04 +0100

    Add commented wireframe for Start method

--- VirusSim/Simulation.cs
+++ VirusSim/Simulation.cs
@@ -25,7 +25,46 @@ namespace VirusSim
             Console.WriteLine($"Infection Turn = {v.TInfect}");
             Console.WriteLine($"Turns          = {v.Turns}");
             Console.WriteLine($"View           = {v.View}");
-            Console.WriteLine($"Save           = {v.Save}");
+            Console.WriteLine($"Save           = {v.Save}\n\n");
+
+            // Debug variable. agentsAlive will have to be a list of some sort
+            int agentsAlive = v.Agents;
+            
+            // Saves the current simulation turn
+            int currentTurn = 1;
+
+            // Game Loop, ends when the user's set number of turns is reached
+            // or if all the simulation agents die.
+            while (currentTurn <= v.Turns && agentsAlive > 0)
+            {
+                // If the current turn equals to the user's set infection turn,
+                // one of the healthy agents is randomly infected.
+                if (currentTurn == v.TInfect)
+                {
+                    
+                }
+
+                // Move every agent that is alive
+
+
+                // In each grid position, if one agent is infected, all other
+                // agents in this position also become infected
+
+
+                // Count Healthy, Infected and Dead agents
+                // If v.Save == True, info is saved to be exported
+
+
+                // Show current turn stats
+                // If v.View == True, update display
+
+
+                // DEBUG LINES
+                Console.WriteLine($"Current Turn: {currentTurn}");
+
+                // Increase current turn value by one
+                currentTurn++;
+            }
         }
     }
 }
