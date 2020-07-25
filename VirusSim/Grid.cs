0fe03a34dba969a942952cf1b7a5a5718cace0a7
AfonsoLage-boop <afomsosantoslage@gmail.com>
2020-07-26 00:45:30 +0100

    Merge fix

--- VirusSim/Grid.cs
+++ VirusSim/Grid.cs
@@ -7,24 +7,6 @@ namespace VirusSim
         private State[,] state;
         private Variables v;
 
-        public int turn;
-
-        public CreateGrid(int size, int agents)
-        {
-            state = new State[size,size];
-
-            for (int i = 0; i < agents; i++)
-            {
-                int aux1 = Random.Next(0, size);
-                int aux2 = Random.Next(0, size);
-
-                state[aux1,aux2] = State.H; 
-                // Arranjar uma forma de os guardar 
-            }
-
-            //Atribuir o state a um dos gajos healthy
-
-        }
         public Grid(Variables v)
         {
             this.v = v;
