ce415c80d5f88fe0025cebbfcb184585a27515be
andrepucas <andre.pucas.santos@gmail.com>
2020-07-26 05:40:30 +0100

    Update InsufArgs message with new //

--- VirusSim/UserInterface.cs
+++ VirusSim/UserInterface.cs
@@ -6,20 +6,20 @@ namespace VirusSim
     {
         public void InsufArgs()
         {
-            Console.WriteLine("\n!! ERROR !!");
-            Console.WriteLine("Insufficient arguments passed in command line.");
-            Console.WriteLine("\nTo start the simulation I need to know:");
-            Console.WriteLine("-N    => grid dimensions (N x N);");
-            Console.WriteLine("-M    => number of agents;");
-            Console.WriteLine("-L    => agents' health (turns)");
-            Console.WriteLine("-Tinf => first infected (turn)");
-            Console.WriteLine("-T    => number of turns");
+            Console.WriteLine("\n// !! ERROR !!");
+            Console.WriteLine("// Insufficient arguments passed in command line.");
+            Console.WriteLine("\n// To start the simulation I need to know:");
+            Console.WriteLine("// -N    => grid dimensions (N x N);");
+            Console.WriteLine("// -M    => number of agents;");
+            Console.WriteLine("// -L    => agents' health (turns)");
+            Console.WriteLine("// -Tinf => first infected (turn)");
+            Console.WriteLine("// -T    => number of turns");
 
-            Console.WriteLine("\nExtras:");
-            Console.WriteLine("-v    => view live simulation");
-            Console.WriteLine("-o    => save simulation data in a .tsv file");
-            Console.WriteLine("\nInput Example:");
-            Console.WriteLine("(-N 50 -M 50 -L 2 -Tinf 5 -T 100 -v -o)");
+            Console.WriteLine("\n// Extras:");
+            Console.WriteLine("// -v    => view live simulation");
+            Console.WriteLine("// -o    => save simulation data in a .tsv file");
+            Console.WriteLine("\n// Input Example:");
+            Console.WriteLine("// (-N 50 -M 50 -L 2 -Tinf 5 -T 100 -v -o)");
         }
 
         public void Start(int size, int agents)
